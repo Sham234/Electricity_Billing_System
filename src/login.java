df4239929a3d3c769f19ab94aaa3ecaa9da60232
Xin <tankaixin02@gmail.com>
2023-01-24 00:23:42 +0800

    Project and login changes

--- src/login.java
+++ src/login.java
@@ -9,11 +9,11 @@ import java.sql.*;
 
 public class login extends JFrame implements ActionListener
 {
-    JLabel l1,l2,l3;
+    JLabel l1,l2,l3,l4;
     JTextField tf1;
     JPasswordField pf2;
-    JButton b1,b2;
-    JPanel panel,p2,p3,p4;
+    JButton b1,b2,b3;
+    JPanel panel,p2,p3,p4,p5,p6,p7;
 
     login()
     {
@@ -22,9 +22,14 @@ public class login extends JFrame implements ActionListener
         p2 = new JPanel();
         p3 = new JPanel();
         p4 = new JPanel();
+        p5 = new JPanel();
+        p6 = new JPanel();
+        p7 = new JPanel();
 
-        l1=new JLabel("User Name");
+        l1=new JLabel("Username");
         l2=new JLabel("Password");
+        l3=new JLabel("Log in");
+        l4=new JLabel("Not yet a member?");
         tf1=new JTextField(15);
         pf2=new JPasswordField(15);
 
@@ -33,7 +38,7 @@ public class login extends JFrame implements ActionListener
         l1.setOpaque(true);
         l1.setForeground(Color.BLACK);
         l1.setBackground(Color.WHITE);
-        l1.setFont(new Font("SansSerif", 1, 12));
+        l1.setFont(new Font("SansSerif", 1, 14));
 
         /* User text field size */
         tf1.setBounds(100,125,193,28);
@@ -42,7 +47,11 @@ public class login extends JFrame implements ActionListener
         l2.setBounds(100,150,70,20);
         l2.setForeground(Color.BLACK);
         l2.setBackground(Color.WHITE);
-        l2.setFont(new Font("SansSerif", 1, 12));
+        l2.setFont(new Font("SansSerif", 1, 14));
+
+        l3.setFont(new Font("Serif",Font.ITALIC,36));
+        //Move the label to center
+        l3.setHorizontalAlignment(JLabel.CENTER);
 
         /* Password field size */
         pf2.setBounds(100,150,193,28);
@@ -54,32 +63,43 @@ public class login extends JFrame implements ActionListener
 //        Image i1=ic1.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
         b1=new JButton("Login");
         b1.setBounds(100,110,90,25);
-        b1.setForeground(Color.BLACK);
-        b1.setBackground(new Color(242, 121, 53));
+        b1.setForeground(Color.WHITE);
+        b1.setBackground(new Color(255, 148, 112));
 
 //        ImageIcon ic2=new ImageIcon(ClassLoader.getSystemResource("images/cancel.png"));
 //        Image i2=ic2.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
         b2=new JButton("Cancel");
         b2.setBounds(350,110,90,25);
-        b2.setForeground(Color.BLACK);
-        b2.setBackground(new Color(242, 121, 53));
+        b2.setForeground(Color.WHITE);
+        b2.setBackground(new Color(255, 148, 112));
+
+        b3=new JButton("Sign up");
+        b3.setBounds(550,110,90,25);
+        b3.setForeground(Color.WHITE);
+        b3.setBackground(new Color(255, 148, 112));
 
         b1.addActionListener(this);
         b2.addActionListener(this);
+        b3.addActionListener(this);
 
-        ImageIcon ic3=new ImageIcon(ClassLoader.getSystemResource("images/login2.png"));
-        Image i3=ic3.getImage().getScaledInstance(200,200,Image.SCALE_SMOOTH);
-        ImageIcon icc3=new ImageIcon(i3);
-
-        l3=new JLabel(icc3);
+//        ImageIcon ic3=new ImageIcon(ClassLoader.getSystemResource("images/login2.png"));
+//        Image i3=ic3.getImage().getScaledInstance(200,200,Image.SCALE_SMOOTH);
+//        ImageIcon icc3=new ImageIcon(i3);
+//
+//        l3=new JLabel(icc3);
 
         setLayout(new BorderLayout(20,20));
 
-        add(l3,BorderLayout.WEST);
         add(p2,BorderLayout.NORTH);
-        add(p3,BorderLayout.AFTER_LINE_ENDS);
+        add(p3,BorderLayout.LINE_START);
+        add(p5,BorderLayout.BEFORE_FIRST_LINE);
         p2.setBackground(Color.WHITE);
         p3.setBackground(Color.WHITE);
+        p5.setBackground(Color.WHITE);
+
+        p6.add(l3);
+        add(p6,BorderLayout.BEFORE_FIRST_LINE);
+        p6.setBackground(new Color(255, 148, 112));
 
         panel.add(l1);
         panel.add(tf1);
@@ -87,14 +107,18 @@ public class login extends JFrame implements ActionListener
         panel.add(pf2);
         p4.add(b1);
         p4.add(b2);
+        p7.add(l4);
+        p7.add(b3);
         add(panel,BorderLayout.CENTER);
         panel.setBackground(Color.WHITE);
 
-        add(p4,BorderLayout.SOUTH);
+        add(p4,BorderLayout.AFTER_LINE_ENDS);
         p4.setBackground(Color.WHITE);
 
-        setSize(new Dimension(500,300));
-        setLocation(new Point(400,200));
+        add(p7,BorderLayout.AFTER_LAST_LINE);
+
+        setSize(new Dimension(480,350));
+        setLocation(new Point(420,170));
         getContentPane().setBackground(Color.WHITE);
         add(panel);
         setVisible(true);
