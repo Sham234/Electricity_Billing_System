cab08d11ad0c4641cc98829757af9fac16a48873
Xin <tankaixin02@gmail.com>
2023-01-23 21:10:42 +0800

    Project and login changes

--- src/Project.java
+++ src/Project.java
@@ -1,55 +1,64 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.Box;
 
 public class Project extends JFrame implements ActionListener{
     Project(){
         super("Electricity Billing System");
 
-        setSize(1500,800);
+        setSize(1200,700);
 
         /* Adding background image */
-        ImageIcon ic =  new ImageIcon(ClassLoader.getSystemResource("images/main.jpg"));
-        Image i3 = ic.getImage().getScaledInstance(1420, 720,Image.SCALE_DEFAULT);
+        ImageIcon ic =  new ImageIcon(ClassLoader.getSystemResource("images/23.png"));
+        Image i3 = ic.getImage().getScaledInstance(1280,660,Image.SCALE_SMOOTH);
         ImageIcon icc3 = new ImageIcon(i3);
         JLabel l1 = new JLabel(icc3);
         add(l1);
 
-        /* First Column */
+        /* Create the menubar */
         JMenuBar mb  = new JMenuBar();
+        mb.setBackground(new Color(242, 121, 53));
+        mb.setPreferredSize(new Dimension(100,50));
+        mb.setOpaque(true);
+
+        /* First Column */
         JMenu master = new JMenu("Master");
         JMenuItem m1 = new JMenuItem("New Customer");
         JMenuItem m2 = new JMenuItem("Customer Details");
         JMenuItem m3 = new JMenuItem("Deposit Details");
-        master.setForeground(Color.BLUE);
+        master.setForeground(Color.BLACK);
+        master.setFont(new Font("serif", Font.BOLD, 15));
 
+        /* ---- New Customer ---- */
+        m1.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon1 = new ImageIcon(ClassLoader.getSystemResource("images/icon1.png"));
+        Image image1 = icon1.getImage().getScaledInstance(25, 25,Image.SCALE_SMOOTH);
+        m1.setIcon(new ImageIcon(image1));
+        m1.setMnemonic('N');
+        m1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        m1.setBackground(Color.WHITE);
+        m1.setForeground(Color.BLACK);
 
         /* ---- Customer Details ---- */
-        m1.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon1 = new ImageIcon(ClassLoader.getSystemResource("images/icon1.jpg"));
-        Image image1 = icon1.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
-        m1.setIcon(new ImageIcon(image1));
-        m1.setMnemonic('D');
-        m1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
-        m1.setBackground(Color.WHITE);
-
-        /* ---- Meter Details ---- */
-        m2.setFont(new Font("monospaced",Font.PLAIN,12));
+        m2.setFont(new Font("serif",Font.PLAIN,13));
         ImageIcon icon2 = new ImageIcon(ClassLoader.getSystemResource("images/icon2.png"));
         Image image2 = icon2.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         m2.setIcon(new ImageIcon(image2));
-        m2.setMnemonic('M');
-        m2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
+        m2.setMnemonic('C');
+        m2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
         m2.setBackground(Color.WHITE);
+        m2.setForeground(Color.BLACK);
 
         /* ---- Deposit Details  ----- */
-        m3.setFont(new Font("monospaced",Font.PLAIN,12));
+        m3.setFont(new Font("serif",Font.PLAIN,13));
         ImageIcon icon3 = new ImageIcon(ClassLoader.getSystemResource("images/icon3.png"));
         Image image3 = icon3.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         m3.setIcon(new ImageIcon(image3));
         m3.setMnemonic('N');
         m3.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
         m3.setBackground(Color.WHITE);
+        m3.setForeground(Color.BLACK);
 
         m1.addActionListener(this);
         m2.addActionListener(this);
@@ -57,17 +66,41 @@ public class Project extends JFrame implements ActionListener{
 
         // --------------------------------------------------------------------------------------------
 
+        /* Spacing Column */
+        JMenu sp1 = new JMenu("");
+        sp1.setEnabled(false);
+        JMenu sp2 = new JMenu("");
+        sp2.setEnabled(false);
+        JMenu sp3 = new JMenu("");
+        sp3.setEnabled(false);
+        JMenu sp4 = new JMenu("");
+        sp4.setEnabled(false);
+        JMenu sp5 = new JMenu("");
+        sp5.setEnabled(false);
+        JMenu sp6 = new JMenu("");
+        sp6.setEnabled(false);
+        JMenu sp7 = new JMenu("");
+        sp7.setEnabled(false);
+//        JMenu sp8 = new JMenu("");
+//        sp8.setEnabled(false);
+//        JMenu sp9 = new JMenu("");
+//        sp9.setEnabled(false);
+//        JMenu sp10 = new JMenu("");
+//        sp10.setEnabled(false);
+
+        // --------------------------------------------------------------------------------------------
 
         /* Second Column */
         JMenu user = new JMenu("User");
         JMenuItem u1 = new JMenuItem("Pay Bill");
         JMenuItem u2 = new JMenuItem("Calculate Bill");
         JMenuItem u3 = new JMenuItem("Last Bill");
-        user.setForeground(Color.RED);
+        user.setForeground(Color.BLACK);
+        user.setFont(new Font("serif", Font.BOLD, 15));
 
         /* ---- Pay Bill ---- */
-        u1.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon4 = new ImageIcon(ClassLoader.getSystemResource("images/icon4.png"));
+        u1.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon4 = new ImageIcon(ClassLoader.getSystemResource("images/icon3.png"));
         Image image4 = icon4.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         u1.setIcon(new ImageIcon(image4));
         u1.setMnemonic('P');
@@ -75,8 +108,8 @@ public class Project extends JFrame implements ActionListener{
         u1.setBackground(Color.WHITE);
 
         /* ---- Bill Details ---- */
-        u2.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon5 = new ImageIcon(ClassLoader.getSystemResource("images/icon5.png"));
+        u2.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon5 = new ImageIcon(ClassLoader.getSystemResource("images/icon4.png"));
         Image image5 = icon5.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         u2.setIcon(new ImageIcon(image5));
         u2.setMnemonic('B');
@@ -84,8 +117,8 @@ public class Project extends JFrame implements ActionListener{
         u2.setBackground(Color.WHITE);
 
         /* ---- Last Bill ----*/
-        u3.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon6 = new ImageIcon(ClassLoader.getSystemResource("images/icon6.png"));
+        u3.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon6 = new ImageIcon(ClassLoader.getSystemResource("images/icon5.png"));
         Image image6 = icon6.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         u3.setIcon(new ImageIcon(image6));
         u3.setMnemonic('L');
@@ -97,20 +130,21 @@ public class Project extends JFrame implements ActionListener{
         u3.addActionListener(this);
 
 
-        // --------------------------------------------------------------------------------------------- 
+        // ---------------------------------------------------------------------------------------------
 
         /* Third Column*/
         JMenu report = new JMenu("Report");
         JMenuItem r1 = new JMenuItem("Generate Bill");
-        report.setForeground(Color.BLUE);
+        report.setForeground(Color.BLACK);
+        report.setFont(new Font("serif", Font.BOLD, 15));
 
         /* ---- Report ---- */
-        r1.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon7 = new ImageIcon(ClassLoader.getSystemResource("images/icon7.png"));
+        r1.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon7 = new ImageIcon(ClassLoader.getSystemResource("images/icon6.png"));
         Image image7 = icon7.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         r1.setIcon(new ImageIcon(image7));
-        r1.setMnemonic('R');
-        r1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK));
+        r1.setMnemonic('G');
+        r1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G, ActionEvent.CTRL_MASK));
         r1.setBackground(Color.WHITE);
 
         r1.addActionListener(this);
@@ -122,29 +156,30 @@ public class Project extends JFrame implements ActionListener{
         JMenuItem ut1 = new JMenuItem("Notepad");
         JMenuItem ut2 = new JMenuItem("Calculator");
         JMenuItem ut3 = new JMenuItem("Web Browser");
-        utility.setForeground(Color.RED);
+        utility.setForeground(Color.BLACK);
+        utility.setFont(new Font("serif", Font.BOLD, 15));
 
-        /* ---- Calender ---- */
-        ut1.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon8 = new ImageIcon(ClassLoader.getSystemResource("images/icon12.png"));
+        /* ---- Notepad ---- */
+        ut1.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon8 = new ImageIcon(ClassLoader.getSystemResource("images/icon7.png"));
         Image image8 = icon8.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         ut1.setIcon(new ImageIcon(image8));
-        ut1.setMnemonic('C');
-        ut1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        ut1.setMnemonic('N');
+        ut1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.CTRL_MASK));
         ut1.setBackground(Color.WHITE);
 
         /* ---- Calculator ---- */
-        ut2.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon9 = new ImageIcon(ClassLoader.getSystemResource("images/icon9.png"));
+        ut2.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon9 = new ImageIcon(ClassLoader.getSystemResource("images/icon8.png"));
         Image image9 = icon9.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         ut2.setIcon(new ImageIcon(image9));
-        ut2.setMnemonic('X');
+        ut2.setMnemonic('C');
         ut2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
         ut2.setBackground(Color.WHITE);
 
         /* ---- Web Browser ---- */
-        ut3.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon10 = new ImageIcon(ClassLoader.getSystemResource("images/icon10.png"));
+        ut3.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon10 = new ImageIcon(ClassLoader.getSystemResource("images/icon9.png"));
         Image image10 = icon10.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         ut3.setIcon(new ImageIcon(image10));
         ut3.setMnemonic('W');
@@ -161,11 +196,12 @@ public class Project extends JFrame implements ActionListener{
         /*Fifth Column */
         JMenu exit = new JMenu("Exit");
         JMenuItem ex = new JMenuItem("Exit");
-        exit.setForeground(Color.BLUE);
+        exit.setForeground(Color.BLACK);
+        exit.setFont(new Font("serif", Font.BOLD, 15));
 
         /* ---- Exit ---- */
-        ex.setFont(new Font("monospaced",Font.PLAIN,12));
-        ImageIcon icon11 = new ImageIcon(ClassLoader.getSystemResource("images/icon11.png"));
+        ex.setFont(new Font("serif",Font.PLAIN,13));
+        ImageIcon icon11 = new ImageIcon(ClassLoader.getSystemResource("images/icon10.png"));
         Image image11 = icon11.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
         ex.setIcon(new ImageIcon(image11));
         ex.setMnemonic('Z');
@@ -193,12 +229,20 @@ public class Project extends JFrame implements ActionListener{
         utility.add(ut3);
 
         exit.add(ex);
-
+        mb.add(sp1);
+        mb.add(sp2);
         mb.add(master);
+        mb.add(sp3);
         mb.add(user);
+        mb.add(sp4);
         mb.add(report);
+        // Separate between two columns
+        mb.add(Box.createHorizontalGlue());
         mb.add(utility);
+        mb.add(sp5);
         mb.add(exit);
+        mb.add(sp6);
+        mb.add(sp7);
 
         setJMenuBar(mb);
 
@@ -241,10 +285,7 @@ public class Project extends JFrame implements ActionListener{
             new LastBill().setVisible(true);
 
         }
-
-
     }
-
 
     public static void main(String[] args){
         new Project().setVisible(true);
--- src/login.java
+++ src/login.java
@@ -2,6 +2,7 @@
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
@@ -12,57 +13,90 @@ public class login extends JFrame implements ActionListener
     JTextField tf1;
     JPasswordField pf2;
     JButton b1,b2;
-    JPanel p1,p2,p3,p4;
+    JPanel panel,p2,p3,p4;
 
     login()
     {
         super("Login Page");
+        panel = new JPanel();
+        p2 = new JPanel();
+        p3 = new JPanel();
+        p4 = new JPanel();
+
         l1=new JLabel("User Name");
         l2=new JLabel("Password");
         tf1=new JTextField(15);
         pf2=new JPasswordField(15);
 
-        ImageIcon ic1=new ImageIcon(ClassLoader.getSystemResource("images/login.jpg"));
-        Image i1=ic1.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
-        b1=new JButton("Login",new ImageIcon(i1));
+        /* User text label size */
+        l1.setBounds(100,25,70,20);
+        l1.setOpaque(true);
+        l1.setForeground(Color.BLACK);
+        l1.setBackground(Color.WHITE);
+        l1.setFont(new Font("SansSerif", 1, 12));
 
-        ImageIcon ic2=new ImageIcon(ClassLoader.getSystemResource("images/cancel.png"));
-        Image i2=ic2.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
-        b2=new JButton("Cancel",new ImageIcon(i2));
+        /* User text field size */
+        tf1.setBounds(100,125,193,28);
+
+        /* Password label size */
+        l2.setBounds(100,150,70,20);
+        l2.setForeground(Color.BLACK);
+        l2.setBackground(Color.WHITE);
+        l2.setFont(new Font("SansSerif", 1, 12));
+
+        /* Password field size */
+        pf2.setBounds(100,150,193,28);
+
+        //-------------------------------------------------------------------------------------------------------
+
+        /* Button */
+//        ImageIcon ic1=new ImageIcon(ClassLoader.getSystemResource("images/login.jpg"));
+//        Image i1=ic1.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
+        b1=new JButton("Login");
+        b1.setBounds(100,110,90,25);
+        b1.setForeground(Color.BLACK);
+        b1.setBackground(new Color(242, 121, 53));
+
+//        ImageIcon ic2=new ImageIcon(ClassLoader.getSystemResource("images/cancel.png"));
+//        Image i2=ic2.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
+        b2=new JButton("Cancel");
+        b2.setBounds(350,110,90,25);
+        b2.setForeground(Color.BLACK);
+        b2.setBackground(new Color(242, 121, 53));
 
         b1.addActionListener(this);
         b2.addActionListener(this);
 
-        ImageIcon ic3=new ImageIcon(ClassLoader.getSystemResource("images/pop.jpg"));
-        Image i3=ic3.getImage().getScaledInstance(340,370,Image.SCALE_DEFAULT);
+        ImageIcon ic3=new ImageIcon(ClassLoader.getSystemResource("images/login2.png"));
+        Image i3=ic3.getImage().getScaledInstance(200,200,Image.SCALE_SMOOTH);
         ImageIcon icc3=new ImageIcon(i3);
 
         l3=new JLabel(icc3);
 
-        setLayout(new BorderLayout());
-
-        p1=new JPanel();
-        p2=new JPanel();
-        p3=new JPanel();
-        p4=new JPanel();
+        setLayout(new BorderLayout(20,20));
 
         add(l3,BorderLayout.WEST);
-        p2.add(l1);
-        p2.add(tf1);
-        p2.add(l2);
-        p2.add(pf2);
-        add(p2,BorderLayout.CENTER);
+        add(p2,BorderLayout.NORTH);
+        add(p3,BorderLayout.AFTER_LINE_ENDS);
+        p2.setBackground(Color.WHITE);
+        p3.setBackground(Color.WHITE);
 
+        panel.add(l1);
+        panel.add(tf1);
+        panel.add(l2);
+        panel.add(pf2);
         p4.add(b1);
         p4.add(b2);
-        add(p4,BorderLayout.SOUTH);
+        add(panel,BorderLayout.CENTER);
+        panel.setBackground(Color.WHITE);
 
-        p2.setBackground(Color.WHITE);
+        add(p4,BorderLayout.SOUTH);
         p4.setBackground(Color.WHITE);
 
-
-        setSize(640,450);
-        setLocation(600,400);
+        setSize(new Dimension(500,300));
+        setLocation(new Point(400,200));
+        getContentPane().setBackground(Color.WHITE);
+        add(panel);
         setVisible(true);
 
     }
