44fc43cb4b5953889da630d23a32a918ca207206
Sam <tmaerran@student.usm.my>
2023-01-24 04:14:04 +0800

    modify meter bar 2

--- src/LastBill.java
+++ src/LastBill.java
@@ -8,7 +8,7 @@ public class LastBill extends JFrame implements ActionListener
     JLabel l1;
     JTextArea t1;
     JButton b1;
-    Choice c1;
+    Choice meternumber;
     JPanel p1;
     LastBill(){
         setSize(500,900);
@@ -18,18 +18,17 @@ public class LastBill extends JFrame implements ActionListener
 
         l1 = new JLabel("Generate Bill");
 
-        c1 = new Choice();
+        meternumber = new Choice();
 
-        c1.add("1001");
-        c1.add("1002");
-        c1.add("1003");
-        c1.add("1004");
-        c1.add("1005");
-        c1.add("1006");
-        c1.add("1007");
-        c1.add("1008");
-        c1.add("1009");
-        c1.add("1010");
+        try {
+            conn c1  = new conn();
+            ResultSet rs = c1.s.executeQuery("select * from emp");
+            while(rs.next()) {
+                meternumber.add(rs.getString("meter"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         t1 = new JTextArea(50,15);
@@ -39,7 +38,7 @@ public class LastBill extends JFrame implements ActionListener
         b1 = new JButton("Generate Bill");
 
         p1.add(l1);
-        p1.add(c1);
+        p1.add(meternumber);
         add(p1,"North");
 
         add(jsp,"Center");
@@ -53,7 +52,7 @@ public class LastBill extends JFrame implements ActionListener
         try{
             conn c = new conn();
 
-            ResultSet rs = c.s.executeQuery("select * from emp where meter="+c1.getSelectedItem());
+            ResultSet rs = c.s.executeQuery("select * from emp where meter="+meternumber.getSelectedItem());
 
             if(rs.next()){
                 t1.append("\n    Customer Name:"+rs.getString("name"));
@@ -69,17 +68,13 @@ public class LastBill extends JFrame implements ActionListener
 
             t1.append("Details of the Last Bills\n\n\n");
 
-            rs = c.s.executeQuery("select * from bill where meter="+c1.getSelectedItem());
+            rs = c.s.executeQuery("select * from bill where meter="+meternumber.getSelectedItem());
 
             while(rs.next()){
                 t1.append("       "+ rs.getString("month") + "           " +rs.getString("amount") + "\n");
             }
 
-
-
-
-
-
+            
         }catch(Exception e){
             e.printStackTrace();
         }
